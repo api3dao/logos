30d90e8c125ae601b9f1766543a2a0ff2a7cfd7b
hiletmis <hiletmis@gmail.com>
2023-11-25 11:24:22 +0300

    Add new files and update dependencies

---
+++ viewer/src/App.js
@@ -0,0 +1,57 @@
+import { VStack, Flex, Text } from '@chakra-ui/react';
+import { ChainIcon, SymbolIcon } from 'react-icons';
+import * as Api3Chains from '@api3/chains';
+import Feeds from './data/feeds.json'
+
+const Welcome = () => {
+
+  const getSymbols = () => {
+    return [...new Set(Feeds.map((feed) => feed.name.split('/')).flat())];
+  }
+
+
+  return (
+    <VStack
+      p={10}
+      bgColor={"white"}
+      borderRadius={'sm'}
+      boxShadow={"md"}
+      spacing={5}
+      width={'450px'}
+      maxWidth={'1000px'}
+      alignItems={'left'}
+      justifyItems={'left'}
+    >
+
+      {
+        getSymbols().map((feed, index) => {
+          return (
+            <Flex p={3} boxShadow={"md"} bgColor={"white"} key={index} alignItems="center" justifyContent="left">
+              <SymbolIcon id={feed} size={50} />
+              <Text fontSize="xl" fontWeight="bold" ml={2}>
+                {"#" + feed + "#"}
+              </Text>
+            </Flex>
+          );
+        })
+      }
+      {
+        Api3Chains.CHAINS.map((chain) => {
+          return (
+            <Flex p={3} boxShadow={"md"} bgColor={"white"} key={chain.name} alignItems="center" justifyContent="left">
+              <ChainIcon id={chain.id} size={50} />
+              <Text fontSize="xl" fontWeight="bold" ml={2}>
+                {chain.name}
+              </Text>
+              <Text fontSize="xl" ml={2}>
+                {chain.id}
+              </Text>
+            </Flex>
+          );
+        })
+      }
+    </VStack>
+  );
+};
+
+export default Welcome;
