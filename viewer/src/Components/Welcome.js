34bd887cf4ddefbf447797f91ac9bf8b224d685a
hiletmis <hiletmis@gmail.com>
2023-11-29 15:16:24 +0300

    Prettier

--- viewer/src/Components/Welcome.js
+++ viewer/src/Components/Welcome.js
@@ -23,8 +23,8 @@ const Welcome = () => {
                 @api3/logos
             </Text>
             <Text fontSize="md" ml={2}>
-                Welcome to @api3/logos package viewer. This package contains logos for chains and symbols that
-                supported by API3
+                Welcome to @api3/logos package viewer. This package contains logos for chains and symbols that supported
+                by API3
             </Text>
             <Tabs selectedIndex={selectedTab} onSelect={(index) => setSelectedTab(index)}>
                 <TabList>
