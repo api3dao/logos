532cd452da6f49def7d05db55b77d4f7d1fc7c40
hiletmis <hiletmis@gmail.com>
2023-12-20 17:51:10 +0300

    Add missing logos to MissingLogosView component

--- viewer/src/Components/MissingLogosView.js
+++ viewer/src/Components/MissingLogosView.js
@@ -1,11 +1,66 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, Image, VStack } from '@chakra-ui/react';
+import * as Api3Chains from '@api3/chains';
+import { SymbolLogoMissing, SymbolLogo, ApiProviderLogoMissing, ApiProviderLogo, ChainLogoMissing, ChainLogo } from 'beta-logos';
+import Title from '../Custom/Title';
+
+const MissingBatchView = ({ header, batch, method }) => {
+
+    const getChain = (chainId) => {
+        return Api3Chains.CHAINS.find((chainObject) => chainObject.id === chainId).name + ' (' + chainId + ')';
+    }
+
+    return (
+        batch.length === 0 ? null :
+            <Flex gap={3} bgColor={'white'} wrap={'wrap'} alignItems="center" justifyContent="left">
+                <VStack width={"100%"} alignItems={"left"}>
+                    <Title header={header} />
+                    <Text fontSize="md" fontWeight="bold" ml={2}>
+                        There is a total of {batch.length} missing {header} logos
+                    </Text>
+                </VStack>
+
+                {batch.map((item, index) => {
+                    return (
+                        <Flex
+                            p={3}
+                            boxShadow={'md'}
+                            width={'150px'}
+                            height={'100px'}
+                            bgColor={'gray.100'}
+                            key={index}
+                            alignItems="center"
+                            justifyContent="left"
+                            cursor={'pointer'}
+                        >
+                            <Image src={method(item)} width={50} height={50} />
+                            <Text fontSize="sm" fontWeight="bold" ml={2}>
+                                {header === 'Chains' ? getChain(item) : item}
+                            </Text>
+                        </Flex>
+                    );
+                })}
+            </Flex>
+    );
+}
 
 const MissingLogosView = () => {
+
+    const checkIfMissingLogos = () => {
+        return SymbolLogoMissing.length + ApiProviderLogoMissing.length + ChainLogoMissing.length;
+    }
+
     return (
         <Flex p={3} gap={3} bgColor={'white'} wrap={'wrap'} alignItems="center" justifyContent="left">
-            <Text fontSize="md" fontWeight="bold" ml={2}>
-                There is a total of 0 missing logos
-            </Text>
+            {
+                checkIfMissingLogos() === 0 ?
+                    <Text fontSize="md" fontWeight="bold" ml={2}>
+                        There is no missing logos
+                    </Text>
+                    : null
+            }
+            <MissingBatchView header={"Symbols"} batch={SymbolLogoMissing} method={SymbolLogo} />
+            <MissingBatchView header={"ApiProviders"} batch={ApiProviderLogoMissing} method={ApiProviderLogo} />
+            <MissingBatchView header={"Chains"} batch={ChainLogoMissing} method={ChainLogo} />
         </Flex>
     );
 };
