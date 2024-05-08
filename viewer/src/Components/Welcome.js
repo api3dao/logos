e6e7114dfe528660487d33e33ce07e06c62c9d9f
hiletmis <hiletmis@gmail.com>
2024-05-08 11:31:25 +0300

    Update Welcome component

--- viewer/src/Components/Welcome.js
+++ viewer/src/Components/Welcome.js
@@ -3,55 +3,26 @@ import ChainsView from './ChainsView';
 import ApiProviderView from './ApiProviderView';
 import MissingLogosView from './MissingLogosView';
 import Docs from './Docs';
-import { VStack, Text, Tab, Tabs, TabList, TabPanel, TabPanels } from '@chakra-ui/react';
+import { VStack } from '@chakra-ui/react';
+import ExpandableView from '../Custom/ExpandableView';
 
 const Welcome = () => {
     return (
         <VStack
-            p={10}
+            p={2}
             bgColor={'white'}
             borderRadius={'sm'}
-            boxShadow="md"
-            spacing={5}
+            spacing={1}
             width={'95vw'}
             maxWidth={'1100px'}
             alignItems={'left'}
             justifyItems={'center'}
-            overflow={'scroll'}
         >
-            <Text fontSize="2xl" fontWeight="bold" ml={2}>
-                @api3/logos
-            </Text>
-            <Text fontSize="md" ml={2}>
-                Welcome to @api3/logos package viewer. This package contains logos for chains and symbols that are
-                supported by API3
-            </Text>
-            <Tabs>
-                <TabList>
-                    <Tab>Symbols</Tab>
-                    <Tab>Chains</Tab>
-                    <Tab>ApiProviders</Tab>
-                    <Tab>Missing</Tab>
-                    <Tab>Docs</Tab>
-                </TabList>
-                <TabPanels>
-                    <TabPanel>
-                        <SymbolsView />
-                    </TabPanel>
-                    <TabPanel>
-                        <ChainsView />
-                    </TabPanel>
-                    <TabPanel>
-                        <ApiProviderView />
-                    </TabPanel>
-                    <TabPanel>
-                        <MissingLogosView />
-                    </TabPanel>
-                    <TabPanel>
-                        <Docs />
-                    </TabPanel>
-                </TabPanels>
-            </Tabs>
+            <ExpandableView header={'Symbols'} view={<SymbolsView />} />
+            <ExpandableView header={'Chains'} view={<ChainsView />} />
+            <ExpandableView header={'ApiProviders'} view={<ApiProviderView />} />
+            <ExpandableView header={'Missing Logos'} view={<MissingLogosView />} />
+            <ExpandableView header={'How to use'} view={<Docs />} />
         </VStack>
     );
 };
